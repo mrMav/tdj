a2598b10a09aac06689c812ca1007490703cd857
mrmav <mrmav@outlook.pt>
2018-05-11 22:30:34 +0100

    Frame and Animation class

---
+++ Game1/Engine/Animations/Animation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Animations
+{
+    /// <summary>
+    /// Represents an Animation.
+    /// </summary>
+    public class Animation
+    {
+        /*
+         * The Sprite parent that this animation belongs to.
+         */ 
+        public Sprite Parent { get; }
+
+        /*
+         * The defined name of this animation.
+         */ 
+        public string Name { get; set; }
+
+        /*
+         * The Frame array of this animation.
+         */ 
+        public Frame[] Frames { get; }
+
+        /*
+         * The delay fo the playback between frames.
+         */
+        public double Delay { get; set; }
+
+        /*
+         * How many times the animation has looped.
+         */
+        public int LoopCount { get; }
+
+        /*
+         * Flags if the animation should loop.
+         */
+        public bool Loop { get; set; }
+
+        /*
+         * Flags if the parent Sprite should be killed on animation complete.
+         */
+        public bool KillOnComplete { get; set; }
+
+        /*
+         * Flags if the animation is currently playng.
+         */
+        public bool IsPlaying { get; }
+
+        /*
+         * The current frame index.
+         */
+        public int FrameIndex { get; }
+
+        /*
+         * Current Frame
+         */
+        public Frame CurrentFrame { get; }
+
+        public Animation(Sprite parent, string name, Frame[] frames, double frameRate = 60, bool loop = true)
+        {
+
+            Parent = parent;
+            Name = name;
+
+            Frames = frames;
+            Delay = 1000 / frameRate;
+
+            Loop = loop;
+
+        }
+    }
+}
---
+++ Game1/Engine/Animations/Frame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Animations
+{
+    /// <summary>
+    /// Represents a single frame.
+    /// </summary>
+    public class Frame
+    {
+        /*
+         * X position in the texture.
+         */
+        public int X { get; set; }
+
+        /*
+         * Y position in the texture.
+         */
+        public int Y { get; set; }
+
+        /*
+         * Width of the frame.
+         */
+        public int Width { get; set; }
+
+        /*
+         * Height of the frame.
+         */
+        public int Height { get; set; }
+
+        /*
+         * Index of this frame in the animation.
+         */
+        public int Index { get; set; }
+        
+        public Frame(int x, int y, int width, int height, int index)
+        {
+
+            X = x;
+            Y = y;
+
+            Width = width;
+            Height = height;
+
+            Index = index;
+
+        }
+    }
+}
